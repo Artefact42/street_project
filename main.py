7ba0b3e84ad10a1c19524497087e9e4590aea7f9
Artefact42 <signeetparaf@protonmail.com>
2022-11-20 18:46:31 +0100

    first test

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import *
+from element import Ligne
+
+try:
+    reset()  
+except Terminator:
+    pass
+
+title("Ma super fenêtre")
+setup(640, 480, 100, 100)
+
+t1 = Ligne(200,42,69,(3,6),"yellow")
+t1.draw()
+
+exitonclick()
