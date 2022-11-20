e9b0bd12b6ffec4c7c2bf0d1f0f4d36ddb429cb5
Artefact42 <signeetparaf@protonmail.com>
2022-11-20 18:05:21 +0100

    update element

--- element.py
+++ element.py
@@ -1,5 +1,24 @@
+from turtle import *
+
 class Ligne :
     def __init__(self,longeur:int,largeur:int,angle:float,origine:tuple,couleur:str):
+        self.l = longeur
+        self.L = largeur
+        self.a = angle
+        self.O = origine
+        self.c = couleur
+    def draw(self):
+        if turtle.isdown() : turtle.penup()
+        turle.setposition(self.O[0], self.O[1])
+        turtle.setheading(self.a)
+        turtle.pencolor(self.c)
+        turtle.pensize(self.L)
+        turtle.pendown()
+        turtle.forward(self.l)
+        turtle.penup()
+
+class arc_cercle :
+    def __init__(self):
         pass
     def draw(self):
         pass
