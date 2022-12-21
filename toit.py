c634b76dcfdecd2c94b84f158a8dfd26ee7a2dbd
Artefact42 <signeetparaf@protonmail.com>
2022-12-21 14:09:28 +0100

    epaisseur à 2

--- toit.py
+++ toit.py
@@ -6,7 +6,7 @@ from triangle import Triangle
 class Toit:
     def __init__(self):
         self.type = randint(1,2)
-        self.e = 1
+        self.e = 2
         self.l = 140
         self.c = "brown"
         self.angle = 45
